53eaf4104e734b20f83bc4666e6e8ad76e239077
Michel Cadennes <michel.cadennes@sens-commun.fr>
2014-08-25 12:27:51 +0200

    Implémentation du bloc Dossiers récents

---
+++ Bundle/DossierRedactionBundle/Controller/PartielsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace TDN\Bundle\DossierRedactionBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use TDN\Bundle\ConseilExpertBundle\Entity\ConseilExpert;
+
+class PartielsController extends Controller {
+	
+	protected $clefs = array(
+		'nos-looks' => array('look', 'style', 'Relooking', 'morphologie', 'star', 'people', 'actrice', 'Blair', 'pretty little liars'),
+		'accessoires-et-shoes' => array('accessoires', 'shoes', 'chaussures', 'bijoux'),
+		'bonnes-affaires' => array( 'bons plans', 'soldes', 'promotions', 'réductions', 'conseils'));
+
+	public function dossiersRecentsAction ($limite = 20, $panel = NULL) {
+
+	    $em = $this->get('doctrine.orm.entity_manager');      
+		$repConseil = $em->getRepository('TDN\Bundle\DossierRedactionBundle\Entity\Dossier');
+		$variables = array();
+		if (is_array($panel)) {
+			$_t = array_intersect($panel, array_keys($this->clefs));
+			$isException = !empty($_t);
+		} else {
+			$isException = in_array($panel, array_keys($this->clefs));	
+		}
+
+		if (!empty($isException)) {
+			$variables['recents'] = $repConseil->findMostRecentWithKeys($limite, $this->clefs[$panel[0]]);
+
+		} else {
+			$variables['recents'] = $repConseil->findMostRecent($limite, $panel);
+		}
+
+		return $this->render('TDNDossierRedactionBundle:Partiels:dossiersRecents.html.twig', $variables);
+	}
+
+}
