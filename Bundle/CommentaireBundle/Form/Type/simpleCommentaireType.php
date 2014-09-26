4c4e3014842d7598e1eb8506be9a93ad2f2a96f0
Michel Cadennes <michel.cadennes@sens-commun.fr>
2014-09-26 13:04:30 +0200

    Fil de commentaire

---
+++ Bundle/CommentaireBundle/Form/Type/simpleCommentaireType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace TDN\Bundle\CommentaireBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository as RubriqueRepository;
+
+class simpleCommentaireType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder->add('texteCommentaire', 'textarea', 
+            array(
+                'label' => '_'
+                ));
+        $builder->add('abonne', 'checkbox', 
+            array(
+                'required' => false,
+                'label' => 'Suivre le fil des commentaires (pour les inscrits)',
+                'attr' => array('class' => 'inline-label')
+            ));
+        $builder->add('idThread', 'hidden', array());
+    }
+
+
+	public function setDefaultOptions(OptionsResolverInterface $resolver) {
+
+	    $resolver->setDefaults(array(
+	    	'data_class' => 'TDN\Bundle\CommentaireBundle\Entity\Commentaire',
+            // 'data_class' => NULL,
+    	));
+	}
+
+    public function getName()
+    {
+        return 'tdn3_commentaire_simple';
+    }
+}
