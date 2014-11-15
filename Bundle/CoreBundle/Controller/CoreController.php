3425f0e8a1b23bffe4d4d3de9c9026bd11aa5998
Michel Cadennes <michel.cadennes@sens-commun.fr>
2014-11-15 12:10:51 +0100

    Page de sommaire des rubriques

--- Bundle/CoreBundle/Controller/CoreController.php
+++ Bundle/CoreBundle/Controller/CoreController.php
@@ -11,8 +11,33 @@ class CoreController extends Controller {
         return $this->render('TDNCoreBundle:Pages:home.html.twig');
     }
 
-    public function sommaireRubriqueAction($slug)
-    {
-        return $this->render('TDNCoreBundle:Pages:rubrique.html.twig', array('rubrique' => $slug));
+    public function sommaireRubriqueAction($slug) {
+
+        $em = $this->get('doctrine.orm.entity_manager');      
+
+        $repRubriques = $em->getRepository('TDN\Bundle\DocumentBundle\Entity\DocumentRubrique');
+        $_r = $repRubriques->findOneBySlug($slug);
+        $variables['rubriqueEntity'] = $_r;
+        $variables['meta_description'] = $_r->getAbstract();
+        if (!is_null($_r->getSponsorLink())) {
+            $variables['sponsorLink'] = $_r->getSponsorLink();
+        }
+        $_ssr = $_r->getSousRubriques();
+        if (count($_ssr) === 0) {
+            $panel = array($slug);
+            $_parent = $_r->getRubriqueParente();
+            if (!empty($_parent)) {
+                $variables['rubriqueEntity'] = $_parent;
+                $variables['rubrique'] = (is_null($_r->getSponsorLink())) ? $_parent->getSlug() : 'sponsored';
+        // print_r($variables['rubrique']); die;
+            }
+        } else {
+            $variables['rubrique'] = (is_null($_r->getSponsorLink())) ? $slug : 'sponsored';
+            foreach ($_ssr as $_r) {
+                $panel[] = $_r->getSlug();
+            }
+        }
+
+        return $this->render('TDNCoreBundle:Pages:rubrique.html.twig', array('panel' => $panel));
     }
 }
